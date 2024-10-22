64d2cb49665ae751b3a3d278d09871b1643e1eb0
Giddeon1 <143545624+Giddeon1@users.noreply.github.com>
2024-10-21 22:04:55 -0400

    test

--- ProjectProgPtrns/src/main/java/org/gigi/Main.java
+++ ProjectProgPtrns/src/main/java/org/gigi/Main.java
@@ -3,5 +3,6 @@ package org.gigi;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello yakin, 2 man when?");
     }
 }
