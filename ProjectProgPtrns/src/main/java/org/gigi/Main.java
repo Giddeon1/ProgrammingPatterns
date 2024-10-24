a0f40245687767b06dbb14b37ff06612cba9233a
Giddeon1 <143545624+Giddeon1@users.noreply.github.com>
2024-10-24 13:01:03 -0400

    added test
    test for git

--- ProjectProgPtrns/src/main/java/org/gigi/Main.java
+++ ProjectProgPtrns/src/main/java/org/gigi/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello yakin, 2 man when?");
+        System.out.println("test");
     }
 }
