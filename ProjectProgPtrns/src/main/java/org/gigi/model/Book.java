ff3cdd3201c095e34bc74ca854534f43d113a929
Giddeon1 <143545624+Giddeon1@users.noreply.github.com>
2024-11-15 21:11:15 -0500

    Implemented Book Class
    created and finished abstract class book
    created regular and irregular book class

---
+++ ProjectProgPtrns/src/main/java/org/gigi/model/Book.java
@@ -0,0 +1,10 @@
+package org.gigi.model;
+
+public abstract class Book {
+    protected String isbn;
+    protected String title;
+    protected String authorFName;
+    protected String authorLName;
+    protected int year;
+    protected int copies;
+}
---
+++ ProjectProgPtrns/src/main/java/org/gigi/model/RegularBook.java
@@ -0,0 +1,4 @@
+package org.gigi.model;
+
+public class RegularBook extends Book {
+}
