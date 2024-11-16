4ea367dfa4856818603af3bef90613566dc5a281
Giddeon1 <143545624+Giddeon1@users.noreply.github.com>
2024-11-15 20:54:18 -0500

    another test
    cleared main

--- ProjectProgPtrns/src/main/java/org/gigi/Main.java
+++ ProjectProgPtrns/src/main/java/org/gigi/Main.java
@@ -2,8 +2,6 @@ package org.gigi;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("hello yakin, 2 man when?");
-        System.out.println("test");
+
     }
 }
