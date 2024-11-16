586e3fa982c7f4013bf82e25d08aa1001acb4694
Giddeon1 <143545624+Giddeon1@users.noreply.github.com>
2024-11-16 16:38:35 -0500

    Edited some classes
    removed phone number, address and dob from user class as we don't need it

--- ProjectProgPtrns/src/main/java/org/gigi/model/Student.java
+++ ProjectProgPtrns/src/main/java/org/gigi/model/Student.java
@@ -6,8 +6,8 @@ public class Student extends User {
     private static int nextId = 1;
     private static final int MAX_BOOKS_ALLOWED = 3;
 
-    public Student(String firstName, String lastName, String email, String phone, String address, LocalDate dateOfBirth) {
-        super(nextId++, firstName, lastName, email, phone, address, dateOfBirth, MAX_BOOKS_ALLOWED);
+    public Student(String firstName, String lastName, String email) {
+        super(nextId++, firstName, lastName, email, MAX_BOOKS_ALLOWED);
     }
 
 
--- ProjectProgPtrns/src/main/java/org/gigi/model/User.java
+++ ProjectProgPtrns/src/main/java/org/gigi/model/User.java
@@ -13,30 +13,33 @@ public abstract class User {
     protected String firstName;
     protected String lastName;
     protected String email;
-    protected String phone;
-    protected String address;
-    protected LocalDate dateOfBirth;
     protected int maxBooksAllowed;
     protected int overDueBookCount;
     protected List<Book> issuedBooks;
 
-    public User(int userId, String firstName, String lastName, String email, String phone, String address, LocalDate dateOfBirth,int maxBooksAllowed) {
+    public User(int userId, String firstName, String lastName, String email,int maxBooksAllowed) {
+        if (!isNameValid(firstName, lastName)) {
+            throw new IllegalArgumentException("Invalid name: Names must contain only letters and spaces.");
+        }
         this.userId = userId;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
-        this.phone = phone;
-        this.address = address;
-        this.dateOfBirth = dateOfBirth;
         this.maxBooksAllowed = maxBooksAllowed;
         this.overDueBookCount = 0;
         this.issuedBooks = new ArrayList<>();
     }
 
-    public String getFullName() {
-        return firstName + " " + lastName;
+    /**
+     * method that checks if the name is not valid(has numbers or special charcters)
+     * @param firstName the firstName of the User
+     * @param lastName the last name of the User
+     * @return true or false if the name is valid or not
+     */
+    private static boolean isNameValid(String firstName, String lastName) {
+        String regex = "^[a-zA-Z\\s]+$"; // Only letters and spaces allowed
+        return firstName != null && lastName != null && firstName.matches(regex) && lastName.matches(regex);
     }
-
 
 
 }
