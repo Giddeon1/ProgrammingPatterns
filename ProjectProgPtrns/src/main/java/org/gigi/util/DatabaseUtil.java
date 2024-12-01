7e9d916a39ee8895be8ab4ac632ae46efc3c2ef4
Giddeon1 <143545624+Giddeon1@users.noreply.github.com>
2024-12-01 14:34:20 -0500

    updated tables
    i did some tables stuff

--- ProjectProgPtrns/src/main/java/org/gigi/util/DatabaseUtil.java
+++ ProjectProgPtrns/src/main/java/org/gigi/util/DatabaseUtil.java
@@ -4,39 +4,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class DatabaseUtil {
+    private static final ReentrantReadWriteLock LOCK = new ReentrantReadWriteLock();
+    private static final ReentrantReadWriteLock.ReadLock READ_LOCK = LOCK.readLock();
+    private static final ReentrantReadWriteLock.WriteLock WRITE_LOCK = LOCK.writeLock();
     /*Missing a lot of stuff in here but will implement after the submission of deliverable 2*/
-
-    public static final String CREATE_STUDENT_TABLE_SQL =
-            """
-            CREATE TABLE IF NOT EXISTS students (
-                id INTEGER PRIMARY KEY,
-                first_name TEXT NOT NULL,
-                last_name TEXT NOT NULL,
-                email TEXT NOT NULL
-            )
-            """;
-
-    public static final String CREATE_STAFF_TABLE_SQL =
-            """
-            CREATE TABLE IF NOT EXISTS staff (
-                id INTEGER PRIMARY KEY,
-                first_name TEXT NOT NULL,
-                last_name TEXT NOT NULL,
-                email TEXT NOT NULL
-            )
-            """;
-
-    public static final String CREATE_LIBRARIAN_TABLE_SQL =
-            """
-            CREATE TABLE IF NOT EXISTS librarian (
-                id INTEGER PRIMARY KEY,
-                first_name TEXT NOT NULL,
-                last_name TEXT NOT NULL,
-                email TEXT NOT NULL
-            )
-            """;
 
     /**
      * establishes a connection to the sqlite database
@@ -58,12 +32,51 @@ public class DatabaseUtil {
      * @param statementStr the SQL statement string to create the table.
      */
     public static void createTable(String statementStr) {
+        WRITE_LOCK.lock();
         try (Connection connection = connect();
              Statement statement = connection.createStatement()) {
             statement.execute(statementStr);
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        } finally {
+            WRITE_LOCK.unlock();
         }
+    }
+
+    public static void CREATE_STUDENT_TABLE_SQL() {
+           String sql = """
+            CREATE TABLE IF NOT EXISTS students (
+                id INTEGER PRIMARY KEY,
+                first_name TEXT NOT NULL,
+                last_name TEXT NOT NULL,
+                email TEXT NOT NULL
+            )
+            """;
+           createTable(sql);
+    }
+
+    public static void CREATE_STAFF_TABLE_SQL() {
+          String sql ="""
+            CREATE TABLE IF NOT EXISTS staff (
+                id INTEGER PRIMARY KEY,
+                first_name TEXT NOT NULL,
+                last_name TEXT NOT NULL,
+                email TEXT NOT NULL
+            )
+            """;
+          createTable(sql);
+    }
+
+    public static void CREATE_LIBRARIAN_TABLE_SQL() {
+        String sql = """
+        CREATE TABLE IF NOT EXISTS librarian (
+        id INTEGER PRIMARY KEY,
+        first_name TEXT NOT NULL,
+        last_name TEXT NOT NULL,
+        email TEXT NOT NULL
+        )
+        """;
+        createTable(sql);
     }
 
     private DatabaseUtil(){}
