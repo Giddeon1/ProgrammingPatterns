11c71d954d28e6e025acc4e5101ff484068abe70
Giddeon1 <143545624+Giddeon1@users.noreply.github.com>
2024-12-10 13:06:33 -0500

    update db methods and library sytem
    i tested all methods in db, removed threadpool to test

--- ProjectProgPtrns/src/main/java/org/gigi/controller/LibrarySystemController.java
+++ ProjectProgPtrns/src/main/java/org/gigi/controller/LibrarySystemController.java
@@ -11,63 +11,59 @@ public class LibrarySystemController {
     private static ExecutorService threadPool = Executors.newFixedThreadPool(10);
 
     public LibrarySystemController() {
-        this.librarySystem = LibrarySystem.getInstance();
         initTables();
+        this.librarySystem = LibrarySystem.getInstance();
     }
 
 
     private void initTables() {
         DatabaseUtil.CREATE_STUDENT_TABLE_SQL();
         DatabaseUtil.CREATE_BOOK_TABLE_SQL();
-       // DatabaseUtil.CREATE_STAFF_TABLE_SQL();
         DatabaseUtil.CREATE_LIBRARIAN_TABLE_SQL();
         DatabaseUtil.CREATE_BORROWED_BOOK_TABLE_SQL();
     }
 
     public void addBook(Book book) {
-        threadPool.submit(() -> {
-            librarySystem.getBooks().add(book);
-            DatabaseUtil.insertIntoBookTable(book);
-        });
+        librarySystem.getBooks().add(book);
+        DatabaseUtil.insertIntoBookTable(book);
     }
 
     public void addUser(User user) {
-        threadPool.submit(() -> {
-            librarySystem.getUsers().add(user);
-            if (user instanceof Student) {
-                DatabaseUtil.insertIntoStudentTable((Student) user);
-            } else if (user instanceof Librarian) {
-                DatabaseUtil.insertIntoLibrarianTable((Librarian) user);
-            }
-        });
+        librarySystem.getUsers().add(user);
+        if (user instanceof Student) {
+            DatabaseUtil.insertIntoStudentTable((Student) user);
+        } else if (user instanceof Librarian) {
+            DatabaseUtil.insertIntoLibrarianTable((Librarian) user);
+        }
     }
 
     public void borrowBook(User user, RegularBook book, Librarian librarian) {
-        threadPool.submit(() -> {
+        //threadPool.submit(() -> {
             if (book.getAvailableCopies() > 0) {
+                book.decrementAvailableCopies();
+                DatabaseUtil.updateBookCopies(book.getIsbn(), book.getAvailableCopies());
                 BorrowedBookRecord record = new BorrowedBookRecord(book, user, librarian);
                 user.borrowBook(record);
                 librarySystem.getBorrowedBookRecords().add(record);
-                book.decrementAvailableCopies();
-                DatabaseUtil.updateBookCopies(book.getIsbn(), book.getAvailableCopies());
+                DatabaseUtil.insertBorrowRecord(record);
             } else {
                 throw new IllegalStateException("No copies of the book are available.");
             }
-        });
+        //});
     }
 
     public void returnBook(User user, RegularBook book) {
-        threadPool.submit(() -> {
+       // threadPool.submit(() -> {
             BorrowedBookRecord record = librarySystem.findBorrowedRecord(user, book);
             if (record == null) {
                 throw new IllegalArgumentException("No borrowed record found for this book and user.");
             }
             record.returnBook();
-            user.returnBook(record);
             book.incrementAvailableCopies();
             DatabaseUtil.updateBookCopies(book.getIsbn(), book.getAvailableCopies());
-            DatabaseUtil.updateReturnDate(book.getIsbn(), user.getUserId());
-        });
+             DatabaseUtil.updateReturnDate(book.getIsbn(), user.getUserId());
+            user.returnBook(record);
+        //});
     }
 
 }
